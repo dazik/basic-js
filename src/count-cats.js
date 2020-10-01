fbd6faaa2a5ed0184a2d6604d5d459c409997d41
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-10-01 22:49:01 +0500

    fix: count cats function fixed

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,9 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(array) {
     let sum = 0;
-    for (i = 0, i < array.length; i++) {
+    for (i = 0; i < array.length; i++) {
         for (k = 0; k < array[i].length; k++) {
-            if (array[i][k] = '^^') {
+            if (array[i][k] === '^^') {
                 sum += 1;
             }
         }

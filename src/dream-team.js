e9d7688689652ebef93aff5c992a6a3a39f09a56
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-10-17 15:04:40 +0500

    init: dream team and what season works now

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray) {
+        Array.isArray = function(arg) {
+            return Object.prototype.toString.call(arg) === '[object Array]';
+        };
+    }
+    if (Array.isArray(members)) {
+        let result = '';
+        for (let i = 0; i < members.length; i++) {
+            if (typeof(members[i]) === 'string') {
+                result = result + /[a-zA-Z]/.exec(members[i]);
+            }
+        }
+        result = result.toUpperCase().split('').sort().join('');
+        return result;
+    } else {
+        return false;
+    }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date === undefined) return 'Unable to determine the time of year!';
+    if (isNaN(Date.parse(date))) throw Error;
+    const checkdate = new Date(Date.parse(date));
+    if (checkdate.getDay() != date.getDay()) throw Error;
+    const month = date.getMonth();
+    switch (true) {
+        case (month < 2) || (month === 11):
+            return 'winter';
+        case (month < 5) && (month > 1):
+            return 'spring';
+        case (month < 8) && (month > 4):
+            return 'summer';
+        case (month < 11) && (month > 7):
+            return 'fall';
+    }
 };

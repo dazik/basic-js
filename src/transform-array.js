e4b794ef48f1867f5f959708d77edc44d0f0d1c6
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-10-31 00:56:42 +0500

    fix: transoft array works, but not at all

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Array.isArray(arr)) {
+        let mod_arr = [];
+        if (arr.length === 0) return [];
+        //console.log(mod_arr);
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i]) {
+                case '--discard-next':
+                    i++;
+                    break;
+                case '--discard-prev':
+                    mod_arr.pop();
+                    break;
+                case '--double-next':
+                    if (arr[i + 1] != undefined) {
+                        mod_arr.push(arr[i + 1]);
+                    };
+                    break;
+                case '--double-prev':
+                    if (arr[i - 1] != undefined) {
+                        mod_arr.push(arr[i - 1]);
+                    }
+                    break;
+                default:
+                    mod_arr.push(arr[i]);
+                    break;
+            };
+        };
+        return mod_arr;
+    } else throw Error;
 };
